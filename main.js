47b37a6b6d20fa903217f10a64b185265592fad7
Skrillx13 <skrillx1300@gmail.com>
2024-11-21 16:15:59 +0800

    Manual styles are hard

---
+++ main.js
@@ -0,0 +1,11 @@
+// This is a comment. It should be greened out.
+
+function specialAdd (a, b) {
+    const SPECIAL_NUMBERS = [4, 8, 12];
+
+    if(SPECIAL_NUMBERS.includes(a)) {
+        return 99;
+    }
+
+    return a + b;
+}
